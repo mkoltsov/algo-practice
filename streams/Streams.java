a3d02ad62a7689c57d862c8450d80b4128b2792e
mkoltsov <root@javabean.ru>
2019-12-01 23:32:17 +0100

    more examples of using streams added

--- streams/Streams.java
+++ streams/Streams.java
@@ -2,61 +2,73 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.IntSupplier;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.*;
 
 public class Streams {
 
-    static class Trader{
+    static class Trader {
         private final String name;
         private final String city;
-        public Trader(String n, String c){
+
+        public Trader(String n, String c) {
             this.name = n;
             this.city = c;
         }
-        public String getName(){
+
+        public String getName() {
             return this.name;
         }
-        public String getCity(){
+
+        public String getCity() {
             return this.city;
         }
-        public String toString(){
-            return "Trader:"+this.name + " in " + this.city;
+
+        public String toString() {
+            return "Trader:" + this.name + " in " + this.city;
         }
     }
 
-     static class Transaction{
+    static class Transaction {
         private final Trader trader;
         private final int year;
         private final int value;
-        public Transaction(Trader trader, int year, int value){
+
+        public Transaction(Trader trader, int year, int value) {
             this.trader = trader;
             this.year = year;
             this.value = value;
         }
-        public Trader getTrader(){
+
+        public Trader getTrader() {
             return this.trader;
         }
-        public int getYear(){
+
+        public int getYear() {
             return this.year;
         }
-        public int getValue(){
+
+        public int getValue() {
             return this.value;
         }
-        public String toString(){
+
+        public String toString() {
             return "{" + this.trader + ", " +
-                    "year: "+this.year+", " +
-                    "value:" + this.value +"}";
+                    "year: " + this.year + ", " +
+                    "value:" + this.value + "}";
         }
     }
 
     public static void main(String... args) {
         Trader raoul = new Trader("Raoul", "Cambridge");
-        Trader mario = new Trader("Mario","Milan");
-        Trader alan = new Trader("Alan","Cambridge");
-        Trader brian = new Trader("Brian","Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
         List<Transaction> transactions = Arrays.asList(
                 new Transaction(brian, 2011, 300),
                 new Transaction(raoul, 2012, 1000),
@@ -66,20 +78,102 @@ public class Streams {
                 new Transaction(alan, 2012, 950)
         );
 
-        System.out.println(transactions.stream().filter(i-> i.year==2011).sorted(Comparator.comparingInt(a -> a.value)).collect(toList()));
-        System.out.println(transactions.stream().map(i->i.trader.city).distinct().collect(toList()));
-        System.out.println(transactions.stream().filter(i->i.trader.city.equals("Cambridge")).sorted(comparing(i-> i.trader.name)).collect(toList()));
-        System.out.println(transactions.stream().map(i->i.trader.name).distinct().sorted(comparing(i-> i)).collect(toList()));
-        System.out.println(transactions.stream().anyMatch(i->i.trader.city.equals("Milan")));
-        System.out.println(transactions.stream().filter(i->i.trader.city.equals("Cambridge")).map(i->i.value).collect(toList()));
-        System.out.println(transactions.stream().map(i->i.value).reduce(0, Integer::max));
+        System.out.println(transactions.stream().filter(i -> i.year == 2011).sorted(Comparator.comparingInt(a -> a.value)).collect(toList()));
+        System.out.println(transactions.stream().map(i -> i.trader.city).distinct().collect(toList()));
+        System.out.println(transactions.stream().filter(i -> i.trader.city.equals("Cambridge")).sorted(comparing(i -> i.trader.name)).collect(toList()));
+        System.out.println(transactions.stream().map(i -> i.trader.name).distinct().sorted(comparing(i -> i)).collect(toList()));
+        System.out.println(transactions.stream().anyMatch(i -> i.trader.city.equals("Milan")));
+        System.out.println(transactions.stream().filter(i -> i.trader.city.equals("Cambridge")).map(i -> i.value).collect(toList()));
+        System.out.println(transactions.stream().map(i -> i.value).reduce(0, Integer::max));
         Optional<Transaction> smallestTransaction =
                 transactions.stream()
                         .reduce((t1, t2) ->
                                 t1.getValue() < t2.getValue() ? t1 : t2);
         Optional<Transaction> smallestTransaction2 =
                 transactions.stream()
-                        .min(comparing(Transaction::getValue));
+                        .min(comparing(Transaction::getValue
+                        ));
+
+        Stream<int[]> pythagoreanTriples =
+                IntStream.rangeClosed(1, 100).boxed()
+                        .flatMap(a ->
+                                IntStream.rangeClosed(a, 100)
+                                        .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
+                                        .mapToObj(b ->
+                                                new int[]{a, b, (int) Math.sqrt(a * a + b * b)})
+                        );
+        pythagoreanTriples.limit(5)
+                .forEach(t ->
+                        System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+
+        Stream<double[]> pythagoreanTriples2 =
+                IntStream.rangeClosed(1, 100).boxed()
+                        .flatMap(a ->
+                                IntStream.rangeClosed(a, 100)
+                                        .mapToObj(
+                                                b -> new double[]{a, b, Math.sqrt(a * a + b * b)})
+                                        .filter(t -> t[2] % 1 == 0));
+
+        Stream<String> stream = Stream.of("Modern ", "Java ", "In ", "Action");
+        stream.map(String::toUpperCase).forEach(System.out::println);
+
+        Stream<String> emptyStream = Stream.empty();
+
+//        Stream<String> values =
+//                Stream.of("config", "home", "user")
+//                        .flatMap(key -> Stream.ofNullable(System.getProperty(key)));
+//
+//
+//        long uniqueWords = 0;
+//        try(Stream<String> lines =
+//                    Files.lines(Paths.get("data.txt"), Charset.defaultCharset())){
+//            uniqueWords = lines.flatMap(line -> Arrays.stream(line.split(" ")))
+//                    .distinct()
+//                    .count();
+//        }
+//        catch(IOException e){
+//
+//        }
+
+
+        Stream.iterate(0, n -> n + 2)
+                .limit(10)
+                .forEach(System.out::println);
+
+        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]}).limit(20).forEach(t -> System.out.println("(" + t[0] + "," + t[1] + ")"));
+
+
+        Stream.iterate(new int[]{0, 1},
+                t -> new int[]{t[1],t[0] + t[1]})
+                .limit(10)
+                .map(t -> t[0])
+                .forEach(System.out::println);
+
+
+//        IntStream.iterate(0, n -> n < 100, n -> n + 4)
+//                .forEach(System.out::println); JAVA 9
+
+//        IntStream.iterate(0, n -> n + 4)
+//                .takeWhile(n -> n < 100)
+
+//                .forEach(System.out::println); JAVA9
+
+        Stream.generate(Math::random)
+                .limit(5)
+                .forEach(System.out::println);
+
+        IntSupplier fib = new IntSupplier(){
+            private int previous = 0;
+            private int current = 1;
+            public int getAsInt(){
+                int oldPrevious = this.previous;
+                int nextValue = this.previous + this.current;
+                this.previous = this.current;
+                this.current = nextValue;
+                return oldPrevious;
+            }
+        };
+        IntStream.generate(fib).limit(10).forEach(System.out::println);
     }
 
 }
