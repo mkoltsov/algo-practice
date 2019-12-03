b682897668ac941102d0bec85d63ec3f213defba
mkoltsov <mikhail.koltsov@appdynamics.com>
2019-12-03 14:12:27 +0100

    collectors added

---
+++ streams/Collectors.java
@@ -0,0 +1,10 @@
+public class Collectors {
+    public static void main(String... args){
+    Comparator<Dish> dishCaloriesComparator =
+    Comparator.comparingInt(Dish::getCalories);
+Optional<Dish> mostCalorieDish =
+    menu.stream()
+        .collect(maxBy(dishCaloriesComparator));
+    }
+
+}
