90418a3a80baaf5127c285f06f82c9359823ad99
mkoltsov <root@javabean.ru>
2019-12-04 01:31:55 +0100

    formatted

--- streams/Collectors.java
+++ streams/Collectors.java
@@ -1,10 +1,12 @@
+import java.util.Comparator;
+
 public class Collectors {
-    public static void main(String... args){
-    Comparator<Dish> dishCaloriesComparator =
-    Comparator.comparingInt(Dish::getCalories);
-Optional<Dish> mostCalorieDish =
-    menu.stream()
-        .collect(maxBy(dishCaloriesComparator));
+    public static void main(String... args) {
+        Comparator<Dish> dishCaloriesComparator =
+                Comparator.comparingInt(Dish::getCalories);
+        Optional<Dish> mostCalorieDish =
+                menu.stream()
+                        .collect(maxBy(dishCaloriesComparator));
     }
 
 }
