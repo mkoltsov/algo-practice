5890e0a17bdfc5f0db29cfd7a9664faaf9cba06d
mkoltsov <root@javabean.ru>
2019-12-10 07:45:51 +0100

    progress saved

--- streams/Collectors.java
+++ streams/Collectors.java
@@ -1,9 +1,8 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.maxBy;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.*;
 
 public class Collectors {
     public static void main(String... args) {
@@ -15,6 +14,82 @@ public class Collectors {
 
         Map<Currency, List<Transaction>> transactionsByCurrencies =
                 transactions.stream().collect(groupingBy(Transaction::getCurrency));
+
+        int totalCalories = menu.stream().collect(summingInt(Dish::getCalories));
+
+        double avgCalories =
+                menu.stream().collect(averagingInt(Dish::getCalories));
+
+        IntSummaryStatistics menuStatistics =
+                menu.stream().collect(summarizingInt(Dish::getCalories));
+
+        String shortMenu = menu.stream().map(Dish::getName).collect(joining());
+
+        String shortMenu = menu.stream().collect(joining());
+
+        String shortMenu = menu.stream().map(Dish::getName).collect(joining(", "));
+
+        int totalCalories = menu.stream().collect(reducing(
+                0, Dish::getCalories, (i, j) -> i + j));
+
+        Optional<Dish> mostCalorieDish =
+                menu.stream().collect(reducing(
+                        (d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2));
+
+        Stream<Integer> stream = asList(1, 2, 3, 4, 5, 6).stream();
+        List<Integer> numbers = stream.reduce(
+                new ArrayList<Integer>(),
+                (List<Integer> l, Integer e) -> {
+                    l.add(e);
+                    return l; },
+                (List<Integer> l1, List<Integer> l2) -> {
+                    l1.addAll(l2);
+                    return l1; });
+
+        int totalCalories = menu.stream().collect(reducing(0,
+                Dish::getCalories,
+                Integer::sum));
+
+        int totalCalories = menu.stream().mapToInt(Dish::getCalories).sum();
+
+        Map<Dish.Type, List<Dish>> dishesByType =
+                menu.stream().collect(groupingBy(Dish::getType));
+
+        public enum CaloricLevel { DIET, NORMAL, FAT }
+        Map<CaloricLevel, List<Dish>> dishesByCaloricLevel = menu.stream().collect(
+                groupingBy(dish -> {
+                    if (dish.getCalories() <= 400) return CaloricLevel.DIET;
+                    else if (dish.getCalories() <= 700) return CaloricLevel.NORMAL;
+                    else return CaloricLevel.FAT;
+                } ));
+
+        Map<Dish.Type, List<Dish>> caloricDishesByType =
+                menu.stream()
+                        .collect(groupingBy(Dish::getType,
+                                filtering(dish -> dish.getCalories() > 500, toList())));
+
+        Map<Dish.Type, List<String>> dishNamesByType =
+                menu.stream()
+                        .collect(groupingBy(Dish::getType,
+                                mapping(Dish::getName, toList())));
+
+
+        Map<String, List<String>> dishTags = new HashMap<>();
+        dishTags.put("pork", asList("greasy", "salty"));
+        dishTags.put("beef", asList("salty", "roasted"));
+        dishTags.put("chicken", asList("fried", "crisp"));
+        dishTags.put("french fries", asList("greasy", "fried"));
+        dishTags.put("rice", asList("light", "natural"));
+        dishTags.put("season fruit", asList("fresh", "natural"));
+        dishTags.put("pizza", asList("tasty", "salty"));
+        dishTags.put("prawns", asList("tasty", "roasted"));
+        dishTags.put("salmon", asList("delicious", "fresh"));
+
+        Map<Dish.Type, Set<String>> dishNamesByType =
+                menu.stream()
+                        .collect(groupingBy(Dish::getType,
+                                flatMapping(dish -> dishTags.get( dish.getName() ).stream(),
+                                        toSet())));
     }
 
 }
