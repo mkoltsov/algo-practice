5a38b479a0dd4ac2774996d757d49930a6172ed3
mkoltsov <root@javabean.ru>
2019-12-19 01:08:53 +0100

    progress saved

--- streams/ParallelStreams.java
+++ streams/ParallelStreams.java
@@ -4,4 +4,43 @@ public class ParallelStreams {
         System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism",
                 "12");
     }
+
+    class WordCounterSpliterator implements Spliterator<Character> {
+        private final String string;
+        private int currentChar = 0;
+        public WordCounterSpliterator(String string) {
+            this.string = string;
+        }
+        @Override
+        public boolean tryAdvance(Consumer<? super Character> action) {
+            action.accept(string.charAt(currentChar++));                      1
+            return currentChar < string.length();                             2
+        }
+        @Override
+        public Spliterator<Character> trySplit() {
+            int currentSize = string.length() - currentChar;
+            if (currentSize < 10) {
+                return null;                                                  3
+            }
+            for (int splitPos = currentSize / 2 + currentChar;
+                 splitPos < string.length(); splitPos++) {                4
+                if (Character.isWhitespace(string.charAt(splitPos))) {        5
+                    Spliterator<Character> spliterator =                      6
+                    new WordCounterSpliterator(string.substring(currentChar,
+                            splitPos));
+                    currentChar = splitPos;                                   7
+                    return spliterator;                                       8
+                }
+            }
+            return null;
+        }
+        @Override
+        public long estimateSize() {
+            return string.length() - currentChar;
+        }
+        @Override
+        public int characteristics() {
+            return ORDERED + SIZED + SUBSIZED + NON-NULL + IMMUTABLE;
+        }
+    }
 }
