0a374569f70029d16e5cd18570ff54dd39268c93
mkoltsov <root@javabean.ru>
2020-01-07 00:37:31 +0100

    cats and mouse added

--- implementation/CatsAndMouse.java
+++ implementation/CatsAndMouse.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class CatsAndMouse {
+    public static void main(String... args) {
+        System.out.println(catAndMouse(1, 2, 3));
+        System.out.println(catAndMouse(1, 3, 2));
+    }
+
+    static String catAndMouse(int x, int y, int z) {
+        int catADistance = Math.abs(z-x);
+        int catBDistance = Math.abs(z-y);
+        return catADistance==catBDistance ? "Mouse C" :  catADistance>catBDistance ? "Cat B" : "Cat A";
+    }
 }
