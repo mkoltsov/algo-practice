d851bd1fe8682bbe725a1798305338a4fcf17eec
mkoltsov <root@javabean.ru>
2020-01-12 02:49:58 +0100

    complettable future chapter finished

--- modernjava/concurrency/CF.java
+++ modernjava/concurrency/CF.java
@@ -11,4 +11,46 @@ class CF {
                 .map(CompletableFuture::join)
                 .collect(toList());
     }
+//optinal executor
+    private final Executor executor =
+            Executors.newFixedThreadPool(Math.min(shops.size(), 100),
+                                     (Runnable r) -> {
+        Thread t = new Thread(r);
+        t.setDaemon(true);
+        return t;
+    }
+
+    CompletableFuture.supplyAsync(() -> shop.getName() + " price is " +
+            shop.getPrice(product), executor);
+);
+//waiting for the future to complete
+    Future<Double> futurePriceInUSD =
+            CompletableFuture.supplyAsync(() -> shop.getPrice(product))
+                    .thenCombine(
+                            CompletableFuture.supplyAsync(
+                                    () ->  exchangeService.getRate(Money.EUR, Money.USD)),
+                            (price, rate) -> price * rate
+                    ))
+            .orTimeout(3, TimeUnit.SECONDS);
+//or providing a default value
+    Future<Double> futurePriceInUSD =
+            CompletableFuture.supplyAsync(() -> shop.getPrice(product))
+                    .thenCombine(
+                            CompletableFuture.supplyAsync(
+                                    () ->  exchangeService.getRate(Money.EUR, Money.USD))
+                                    .completeOnTimeout(DEFAULT_RATE, 1, TimeUnit.SECONDS),      1
+            (price, rate) -> price * rate
+        ))
+                .orTimeout(3, TimeUnit.SECONDS);
+
+//printing out intemediate values
+    long start = System.nanoTime();
+    CompletableFuture[] futures = findPricesStream("myPhone27S")
+            .map(f -> f.thenAccept(
+                    s -> System.out.println(s + " (done in " +
+                            ((System.nanoTime() - start) / 1_000_000) + " msecs)")))
+            .toArray(size -> new CompletableFuture[size]);
+CompletableFuture.allOf(futures).join();
+System.out.println("All shops have now responded in "
+        + ((System.nanoTime() - start) / 1_000_000) + " msecs");
 }
