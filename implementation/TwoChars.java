8d7c651f429cbc7070fde93a0c74d001dadc6def
mkoltsov <root@javabean.ru>
2021-01-27 01:45:17 +0100

    two chars max points

--- implementation/TwoChars.java
+++ implementation/TwoChars.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class TwoChars {
     public static void main(String[] args) {
@@ -9,35 +10,57 @@ public class TwoChars {
     // Complete the alternate function below.
     static int alternate(String s) {
         int[] dist = s.chars().distinct().toArray();
-        Set<int[]> pairs = new HashSet<>();
-        int numToDelete = dist.length-2;
 
-        for (int i =0; i<dist.length;i++){
-            for (int j=i; j<dist.length;j++){
-                if (dist[i]== dist[j]){
-                    continue;
+        int n = dist.length, max = 0, k = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i+1; j < n; j++)
+            {
+                String temp = "[^" + (char)dist[i] + (char)dist[j] + "]";
+                String f = s.replaceAll(temp,"");
+
+                for(k = 0; k < f.length()-1; k++)
+                {
+                    if(f.charAt(k) == f.charAt(k+1))
+                        break;
                 }
-                pairs.add(new int[]{dist[i], dist[j]});
+
+                if(k == f.length()-1)
+                    max = Math.max(f.length(),max);
             }
         }
-        AtomicInteger maxSize = new AtomicInteger();
-        pairs.forEach(i-> {
-            String replaced = s.replaceAll(String.valueOf((char) i[0]), "").replaceAll(String.valueOf((char) i[1]), "");
-            System.out.println(replaced);
-            System.out.println(isAlternating(replaced));
-                    if (replaced.chars().distinct().count()==2 && isAlternating(replaced)){
-                            if (maxSize.intValue()<replaced.length()){
-                                System.out.println(111+replaced + replaced.chars().distinct().count());
-                                maxSize.set(replaced.length());
-                            }
-                    }
-        });
-        System.out.println(maxSize.intValue());
-//            pairs.to
+        return max;
 
+//        IntStream.range(0, dist.length).forEachOrdered(i->{cc[i]=(char) dist[i];});
+////        System.out.println(11+Arrays.stream(dist).map(i->(char)i).);
+//        permutation(String.valueOf(cc));
+//        for (int i =0; i<dist.length;i++){
+//            for (int j=i; j<dist.length;j++){
+//                if (dist[i]== dist[j]){
+//                    continue;
+//                }
+//                pairs.add(new int[]{dist[i], dist[j]});
+//            }
 //        }
-//        pairs.forEach(i-> System.out.println(Arrays.toString(i)));
-        return 0;
+//        AtomicInteger maxSize = new AtomicInteger();
+//        pairs.forEach(i-> {
+//            String replaced = s.replaceAll(String.valueOf((char) i[0]), "").replaceAll(String.valueOf((char) i[1]), "");
+//            System.out.println(replaced);
+//            System.out.println(isAlternating(replaced));
+//                    if (replaced.chars().distinct().count()==2 && isAlternating(replaced)){
+//                            if (maxSize.intValue()<replaced.length()){
+//                                System.out.println(111+replaced + replaced.chars().distinct().count());
+//                                maxSize.set(replaced.length());
+//                            }
+//                    }
+//        });
+//        System.out.println(maxSize.intValue());
+////            pairs.to
+//
+////        }
+////        pairs.forEach(i-> System.out.println(Arrays.toString(i)));
+//        return 0;
     }
 
     static boolean isAlternating(String str){
@@ -53,5 +76,18 @@ public class TwoChars {
             }
         }}
         return true;
+    }
+
+    public static void permutation(String str) {
+        permutation("", str);
+    }
+
+    private static void permutation(String prefix, String str) {
+        int n = str.length();
+        if (n == 0) System.out.println(prefix);
+        else {
+            for (int i = 0; i < n; i++)
+                permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+        }
     }
 }
