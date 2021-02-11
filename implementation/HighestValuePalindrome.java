34b2bb148d89eca6978a246699241aee8e2ff92c
mkoltsov <root@javabean.ru>
2021-02-11 10:59:56 +0100

    highest value palindrome committed

--- implementation/HighestValuePalindrome.java
+++ implementation/HighestValuePalindrome.java
@@ -8,37 +8,107 @@ public class HighestValuePalindrome {
     }
 
     static String highestValuePalindrome(String s, int n, int k) {
-        char[] chars = s.toCharArray();
-        List<Integer> nonComplaint= new ArrayList<>();
-        for (int i=0;i<s.length()/2;i++){
-            int left=Integer.parseInt(String.valueOf(chars[i])), right=Integer.parseInt(String.valueOf(chars[(s.length()-i)-1]));
-            System.out.printf("%d -%d %n",left,right);
-            if (left!=right){
-                nonComplaint.add(i);
-            }
-        }
-        System.out.println(nonComplaint.size());
-        if (k-nonComplaint.size()<0){
+
+        if(s == null || s.isEmpty()) {
             return "-1";
         }
 
-        for (int i:nonComplaint){
-            int left=Integer.parseInt(String.valueOf(chars[i])), right=Integer.parseInt(String.valueOf(chars[(s.length()-i)-1]));
+        int changes = 0;
+        char[] text = s.toCharArray();
 
-        }
-//            int left=Integer.parseInt(String.valueOf(chars[i])), right=Integer.parseInt(String.valueOf(chars[(s.length()-i)-1]));
-//            if (left==right && k-2>=0){
-//                chars[i]='9';
-//                chars[(s.length()-i)-1]='9';
-//                k-=2;
-//
-//            } else if (left != right && k-2>=0){
-//                chars[i]='9';
-//                chars[(s.length()-i)-1]='9';
-//                k-=2;
-//            } else if (left != right && k-2>=0){
-        return String.valueOf(nonComplaint.size());
+        for (int i = 0, j = n - 1; i < j; i++, j--) {
+            if(text[i] != text[j]) {
+                changes++;
+
+                if(changes > k) {
+                    return "-1";
+                }
+            }
         }
 
+        if(k == 0) {
+            return s;
+        }
+
+        int extraChanges = k - changes;
+
+        for (int i = 0, j = n - 1; i < j; i++, j--) {
+            if(text[i] == text[j]) {
+                if(extraChanges >= 2 && text[i] != '9') {
+                    text[i] = '9';
+                    text[j] = '9';
+                    extraChanges-= 2;
+                }
+            } else {
+                if(extraChanges > 0) {
+                    if(text[i] != '9' && text[j] != '9') {
+                        text[i] = '9';
+                        text[j] = '9';
+                        extraChanges--;
+                    } else if (text[i] == '9' || text[j] == '9') {
+                        text[i] = '9';
+                        text[j] = '9';
+                    }
+                } else {
+                    char max = (char)Math.max(s.charAt(i), s.charAt(j));
+                    text[i] = max;
+                    text[j] = max;
+                }
+            }
+        }
+
+        if(extraChanges > 0 && text.length%2 == 1) {
+            text[text.length/2] = '9';
+        }
+
+        return new String(text);
     }
+//        int i = 0, j = n - 1, count = 0;
+//        string visited = s;
+//        while(i < j){
+//            if(s[i] < s[j]){
+//                s[i] = s[j];
+//                visited[i] = 'x';
+//                count++;
+//            }
+//            else if(s[i] > s[j]){
+//                s[j] = s[i];
+//                visited[j] = 'x';
+//                count++;
+//            }
+//            i++;
+//            j--;
+//        }
+//        int z = k - count;
+//        if(z < 0){
+//            return "-1";
+//        }
+//        else if(z == 0){
+//            return s;
+//        }
+//
+//        i = 0; j = n - 1;
+//        while(z > 0 && i < j){
+//            if(s[i] != '9'){
+//                if(visited[i] == 'x' || visited[j] == 'x'){
+//                    s[i] = '9';
+//                    s[j] = '9';
+//                    z -= 1;
+//                }
+//                else if(z > 1){
+//                    s[i] = '9';
+//                    s[j] = '9';
+//                    z -= 2;
+//                }
+//            }
+//            i++;
+//            j--;
+//        }
+//        if(z > 0 && n % 2 != 0){
+//            s[n/2] = '9';
+//        }
+//        return s;
+        }
+
+//    }
 //}
