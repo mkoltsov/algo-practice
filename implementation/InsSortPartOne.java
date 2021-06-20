6c92f1fac6b28e1e9d63c5e121a55da94267cfa5
mkoltsov <root@javabean.ru>
2021-06-21 01:42:46 +0200

    remember back

---
+++ implementation/InsSortPartOne.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class InsSortPartOne {
+    public static void main(String[] args) {
+        insertionSort1(5, Arrays.asList(1,2,4,5,3));
+    }
+
+    public static void insertionSort1(int n, List<Integer> arr) {
+
+        for (int i=n-1; i!=0; i--){
+            if (arr.get(i)<arr.get(i-1)){
+                int temp = arr.get(i);
+                arr.set(i, arr.get(i-1));
+                arr.set(i-1, temp);
+                System.out.println(arr);
+            }
+
+        }
+
+        System.out.println(arr);
+    }
+}
