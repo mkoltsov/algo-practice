764cd60b5ac554955ea4f850362a351cfe0dee0d
mkoltsov <root@javabean.ru>
2022-02-16 00:28:29 +0100

    getting back to leetcode

--- leetcode/topInterviewQuestions/easy/RemoveDuplicatesFromArray.java
+++ leetcode/topInterviewQuestions/easy/RemoveDuplicatesFromArray.java
@@ -3,8 +3,20 @@ package topInterviewQuestions.easy;
 public class RemoveDuplicatesFromArray {
     public static void main(String[] args) {
 
+        System.out.println(removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
+        System.out.println(removeDuplicates(new int[]{1,1,2}));
     }
-        public int removeDuplicates(int[] nums) {
-            for (int i=0;i<nums.length;i++)
+
+    public static int removeDuplicates(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i] == nums[i + 1]) {
+//                System.out.println("duplicate");
+            } else {
+                k++;
+            }
         }
+        return k+1;
+    }
+
 }
