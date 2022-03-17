9df647e1d94c0ea27c7c6188d1cfbb6f96957952
mkoltsov <root@javabean.ru>
2022-03-17 01:09:12 +0100

    progress saved

--- leetcode/topInterviewQuestions/easy/RemoveDuplicatesFromArray.java
+++ leetcode/topInterviewQuestions/easy/RemoveDuplicatesFromArray.java
@@ -6,21 +6,29 @@ import java.util.stream.IntStream;
 public class RemoveDuplicatesFromArray {
     public static void main(String[] args) {
 
-        System.out.println(removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
-        System.out.println(removeDuplicates(new int[]{1,1,2}));
+//        System.out.println(removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
+//        System.out.println(removeDuplicates(new int[]{1, 1, 2}));
+        System.out.println(removeDuplicates(new int[]{1, 1}));
     }
 
     public static int removeDuplicates(int[] nums) {
-        int[] n = IntStream.of(nums).distinct().toArray();
-        System.out.println(Arrays.toString(n));
         int k = 0;
-        for (int i = 0; i < nums.length-1; i++) {
-            if (nums[i] == nums[i + 1]) {
-//                System.out.println("duplicate");
-            } else {
-                k++;
+        int currentIndex = 0;
+        int currentNumber = nums[k];
+
+        while (currentIndex != nums.length - 1) {
+            currentIndex++;
+            if (currentNumber == nums[currentIndex] && currentIndex<nums.length - 1) {
+                while (currentNumber == nums[currentIndex]) {
+                    currentIndex++;
+                }
             }
+            k++;
+            currentNumber = nums[currentIndex];
+            nums[k] = currentNumber;
         }
+        System.out.println(Arrays.toString(nums));
+
         return k+1;
     }
 
